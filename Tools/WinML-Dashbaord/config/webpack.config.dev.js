a9dfff71647b5f36b1439074208dfa6ba867d3ae
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-06-28 21:52:25 -0700

    Remove html-loader

--- Tools/WinML-Dashbaord/config/webpack.config.dev.js
+++ Tools/WinML-Dashbaord/config/webpack.config.dev.js
@@ -189,16 +189,6 @@ module.exports = {
               },
             ],
           },
-          {
-            test: /\.html$/,
-            exclude: /node_modules/,
-            use: {
-              loader: require.resolve('html-loader'),
-              options: {
-                attrs: ['img:src', 'link:href', 'script:src'],
-              },
-            },
-          },
           // "file" loader makes sure those assets get served by WebpackDevServer.
           // When you `import` an asset, you get its (virtual) filename.
           // In production, they would get copied to the `build` folder.
--- Tools/WinML-Dashbaord/config/webpack.config.prod.js
+++ Tools/WinML-Dashbaord/config/webpack.config.prod.js
@@ -212,17 +212,6 @@ module.exports = {
             ),
             // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
           },
-          {
-            test: /\.html$/,
-            exclude: /node_modules/,
-            use: {
-              loader: require.resolve('html-loader'),
-              options: {
-                minimize: true,
-                attrs: ['img:src', 'link:href', 'script:src'],
-              },
-            },
-          },
           // "file" loader makes sure assets end up in the `build` folder.
           // When you `import` an asset, you get its filename.
           // This loader doesn't use a "test" so it will catch all modules

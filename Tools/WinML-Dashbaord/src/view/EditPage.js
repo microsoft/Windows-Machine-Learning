e9a1de6353dd337ffcb182a67f91de87d06b1379
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-07-02 10:50:40 -0700

    Add panels

--- Tools/WinML-Dashbaord/src/view/EditPage.js
+++ Tools/WinML-Dashbaord/src/view/EditPage.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import { Label } from 'office-ui-fabric-react/lib/Label';
 import './EditPage.css';
 
 export default class EditPage extends Component {
   render() {
     return (
-      <div class="EditPage" style={{height: '100vh'}}>
-        <object class="Netron" type="text/html" data="static/Netron/" width="100%" height="100%">
+      <div className='EditPage'>
+        <div className='Panel'>
+          <Label>Left panel</Label>
+        </div>
+        <object className='Netron' type='text/html' data='static/Netron/'>
             Netron visualization
         </object>
+        <div className='Panel'>
+          <Label>Right panel</Label>
+        </div>
       </div>
     );
   }

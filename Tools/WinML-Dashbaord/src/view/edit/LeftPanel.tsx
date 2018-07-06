54f969f2ff41fd1224e5e289ded36da5e2cf2c70
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-07-06 14:51:33 -0700

    Make panels more responsive

--- Tools/WinML-Dashbaord/src/view/edit/LeftPanel.tsx
+++ Tools/WinML-Dashbaord/src/view/edit/LeftPanel.tsx
@@ -8,31 +8,30 @@ import './Panel.css';
 
 export default class LeftPanel extends React.Component {
     render() {
-        const inputs = [], outputs = [];
-        inputs.push('hi');
-        outputs.push('hi');
+        const inputs = [];
+        inputs.push('stub');
         const inputsForm = [];
-        // for (const input of inputs) {
         for (const _ of inputs) {
             inputsForm.push(<div>
-                <TextField placeholder='N' className='ShapeTextField'>
-                </TextField>
-                <TextField placeholder='C' className='ShapeTextField'>
-                </TextField>
-                <TextField placeholder='H' className='ShapeTextField'>
-                </TextField>
-                <TextField placeholder='W' className='ShapeTextField'>
-                </TextField>
+                <Label className='TensorName'>data_0</Label>
+                <span className='Shape'>
+                    <TextField inputMode='numeric' type='number' placeholder='N' className='ShapeTextField' />
+                    <TextField inputMode='numeric' type='number' placeholder='C' className='ShapeTextField' />
+                    <TextField inputMode='numeric' type='number' placeholder='H' className='ShapeTextField' />
+                    <TextField inputMode='numeric' type='number' placeholder='W' className='ShapeTextField' />
+                </span>
             </div>);
         }
         return (
-            <div className='Panel'>
+            <div>
                 <Label className='PanelName'>Layer</Label>
-                <Collapsible label='Tensor shapes'>
-                    <Label>Inputs</Label>
-                    {inputsForm}
-                    <Label>Outputs</Label>
-                </Collapsible>
+                <div className='Panel'>
+                    <Collapsible label='Tensor shapes'>
+                        <Label>Inputs</Label>
+                        {inputsForm}
+                        <Label>Outputs</Label>
+                    </Collapsible>
+                </div>
             </div>
         );
     }

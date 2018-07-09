5066389e7715a64a5e7c8debd2e9b9a22842af20
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-07-09 16:28:09 -0700

    Ignore leading slash at folder references

--- Tools/WinML-Dashbaord/src/electron.js
+++ Tools/WinML-Dashbaord/src/electron.js
@@ -8,11 +8,16 @@ let mainWindow;
 function createWindow() {
     const fileProtocol = 'file';
     protocol.interceptFileProtocol(fileProtocol, (request, callback) => {
+        // A reference to a folder should return its index.html file
         const filePath = new url.URL(request.url).pathname;
         let resolvedPath = path.normalize(filePath);
+        if (resolvedPath[0] === '\\') {
+            // Remove host to pathname separator
+            resolvedPath = resolvedPath.substr(1);
+        }
         try {
-            if (resolvedPath.slice(-1) === '/' || fs.statSync(resolvedPath).isDirectory) {
-                let index = path.join(resolvedPath, 'index.html');
+            if (resolvedPath.slice(-1) === path.sep || fs.statSync(resolvedPath).isDirectory) {
+                let index = path.posix.join(resolvedPath, 'index.html');
                 if (fs.existsSync(index)) {
                     resolvedPath = index;
                 }
@@ -41,8 +46,7 @@ function createWindow() {
     if (!pageUrl.includes('://')) {
         pageUrl = url.format({
             pathname: path.join(__dirname, '../build/index.html'),
-            protocol: 'file:',
-            slashes: true,
+            protocol: 'file',
         });
     }
     mainWindow.loadURL(pageUrl);

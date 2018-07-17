c148c73b47ffffb7d46449ce02aab0dfe89d5573
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-07-16 17:02:42 -0700

    Use hard links to Netron

--- Tools/WinML-Dashbaord/scripts/postinstall.js
+++ Tools/WinML-Dashbaord/scripts/postinstall.js
@@ -1,3 +1,13 @@
 const fs = require('fs');
+const path = require('path');
 
-fs.symlinkSync('node_modules/netron', '../static/Netron')
+for (directory of ['node_modules/netron', 'node_modules/netron/src']) {
+    if (!fs.existsSync(directory))
+        fs.mkdirSync(directory);
+}
+
+for (source of ['view-render.css', 'view-sidebar.css', 'view.css']) {
+    destination = path.join('node_modules/netron/src', source);
+    if (!fs.existsSync(destination))
+        fs.linkSync(path.join('static/Netron/src', source), path.join('node_modules/netron/src', source));
+}

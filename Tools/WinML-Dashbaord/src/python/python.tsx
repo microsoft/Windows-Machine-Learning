627761660feca7ce54c389b994e4833d689fbb27
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-08-03 17:41:21 -0700

    Use yauzl instead of unzipper

--- Tools/WinML-Dashbaord/src/python/python.tsx
+++ Tools/WinML-Dashbaord/src/python/python.tsx
@@ -1,12 +1,12 @@
 import { execFile, execFileSync } from 'child_process';
+import * as fs from 'fs';
 import * as https from 'https';
 import * as path from 'path';
-import * as unzipper from 'unzipper';
-import * as util from 'util';
+import { promisify } from 'util';
+import * as yauzl from 'yauzl';
 
 import { mkdir, winmlDataFoler } from '../persistence/appData';
 
-const execFilePromisse = util.promisify(execFile);
 const venv = path.join(winmlDataFoler, 'venv');
 
 function filterPythonBinaries(binaries: string[]) {
@@ -30,35 +30,63 @@ export function getPythonBinaries() {
     return binaries;
 }
 
-function downloadPython(success: () => void, error: () => void) {
+function unzip(buffer: Buffer, directory: string) {
+    yauzl.fromBuffer(buffer, (err, zipfile) => {
+        if (err) {
+            // TODO
+            throw err;
+        }
+        zipfile!.readEntry();
+        zipfile!.on('entry', entry => {
+            zipfile!.openReadStream(entry, (error, readStream) => {
+                if (error) {
+                    // TODO
+                    throw error;
+                }
+                readStream!.on('end', zipfile!.readEntry);
+                readStream!.pipe(fs.createWriteStream(path.join(directory, entry.fileName)));
+            });
+        });
+    });
+}
+
+export function downloadPython(success: () => void, error: () => void) {
     if (process.platform !== 'win32') {
         throw new Error('Unsupported platform');
     }
     const pythonUrl = 'https://www.python.org/ftp/python/3.7.0/python-3.7.0-embed-amd64.zip';
     const pythonFolder = mkdir(winmlDataFoler, 'python');
-    https.get(pythonUrl, response =>
-        response.pipe(unzipper.Extract({ path: pythonFolder }))
-        .on('finish', success)
-        .on('error', error));
+    const data: Buffer[] = [];
+    https.get(pythonUrl, response => {
+        response
+        .on('data', (chunk: Buffer) => data.push(chunk))
+        .on('end', () => unzip(Buffer.concat(data), pythonFolder))
+        .on('error', (err: Error) => {
+            // TODO
+            throw err;
+        });
+    });
+}
+
+export function getVenvPython() {
+    // Get Python installed in local venv
+    const binaries = filterPythonBinaries([path.join(venv, 'Scripts/python'), path.join(venv, 'bin/python')]);
+    if (binaries === []) {
+        return null;
+    }
+    return binaries[0];
 }
 
 export async function python(...command: string[]) {
-    const binary = filterPythonBinaries([path.join(venv, 'Scripts/python'), path.join(venv, 'bin/python')])[0];
-    return await execFilePromisse(binary, ['-m', ...command]);
+    return await promisify(execFile)(getVenvPython()!, ['-m', ...command]);
 }
 
 export async function pip(...command: string[]) {
     return await python('pip', ...command);
 }
 
-async function installVenv(targetPython: string) {
-    await execFilePromisse(targetPython, ['-m', 'venv', venv]);
+export async function installVenv(targetPython: string) {
+    await promisify(execFile)(targetPython, ['-m', 'venv', venv]);
     await pip('install', '-U', 'pip');
     await pip('install', '-r', path.join(__dirname, 'requirements.txt'));
 }
-
-module.exports = {
-    downloadPython,
-    getPythonBinaries,
-    installVenv,
-};

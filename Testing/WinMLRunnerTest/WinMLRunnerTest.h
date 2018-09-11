15791dcc540197ee26101ba6359cd42d07e2788a
Ryan Lai <rylai@microsoft.com>
2018-09-11 11:36:12 -0700

    Added taef test project to build taef test dll

---
+++ Testing/WinMLRunnerTest/WinMLRunnerTest.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <WexTestClass.h>
+
+class WinMLRunnerTest
+{
+public:
+    TEST_CLASS(WinMLRunnerTest);
+    TEST_METHOD(TestPrintUsage);
+    //TEST_METHOD(GarbageInputCpuAndGpu);
+    //TEST_METHOD(GarbageInputOnlyCpu);
+    //TEST_METHOD(GarbageInputOnlyCPU);
+    //TEST_METHOD(ProvidedInputCpuAndGpu);
+    //TEST_METHOD(ProvidedInputOnlyCpu);
+    //TEST_METHOD(ProvidedInputOnlyCPU);
+};

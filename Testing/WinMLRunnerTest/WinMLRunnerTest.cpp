34c46c19cae30af5d9c63600d75037c59e22cdbe
Patrice Vignola <pavignol@microsoft.com>
2018-10-10 15:45:05 -0700

    Revert temporary code

--- Testing/WinMLRunnerTest/WinMLRunnerTest.cpp
+++ Testing/WinMLRunnerTest/WinMLRunnerTest.cpp
@@ -347,10 +347,7 @@ namespace WinMLRunnerTest
             std::wstring command = curPath +
                 L"./WinMLRunner " + L"-model " + curPath + L"SqueezeNet.onnx "
                 + L" -input " + curPath + L"horizontal-crop.png";
-
-            auto abc = RunProc((wchar_t *)command.c_str());
-
-            Assert::AreNotEqual(0, abc);
+            Assert::AreNotEqual(0, RunProc((wchar_t *)command.c_str()));
         }
     };
 

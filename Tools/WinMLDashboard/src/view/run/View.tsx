79a538f7303e0b041337efc9f940f3e1a0171b45
Xiang Zhang <xianz@microsoft.com>
2018-11-06 14:39:33 -0800

    basic support for integrating model runner

--- Tools/WinMLDashboard/src/view/run/View.tsx
+++ Tools/WinMLDashboard/src/view/run/View.tsx
@@ -5,12 +5,14 @@ import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
 
 import IState from '../../datastore/state';
 
-
+import { Spinner } from 'office-ui-fabric-react/lib/Spinner';
 import { TextField } from 'office-ui-fabric-react/lib/TextField';
 import { packagedFile } from '../../native/appData';
 import { showNativeOpenDialog } from '../../native/dialog';
 import { execFilePromise } from '../../native/python';
-import './view.css';
+import './View.css';
+
+import Collapsible from '../../components/Collapsible';
 
 const modelRunnerPath = packagedFile('WinMLRunner.exe');
 
@@ -18,29 +20,60 @@ interface IComponentProperties {
     file: File
 }
 
+enum Step {
+    Idle,
+    Running,
+    Success,
+}
+
 interface IComponentState {
     console: string,
+    currentStep: Step,
     input: string,
     model: string,
+
+
 }
 class RunView extends React.Component<IComponentProperties, IComponentState> {
     constructor(props: IComponentProperties) {
         super(props);
         this.state = {
             console: '',
+            currentStep: Step.Idle,
             input: '',
             model: ''
         }
     }
     public render() {
+        const collabsibleRef: React.RefObject<Collapsible> = React.createRef();
         return (
             <div className='RunView'>
-                <div className='ModelPath'>
-                    <TextField id='modelToRun' placeholder='Model Path' value={this.state.model || this.props.file && this.props.file.path} label='Model to convert' onChanged={this.setModel} />
+                <div className='RunViewControls'>
+                    {this.getView()}
+                </div>
+                { this.state.console &&
+                    <Collapsible ref={collabsibleRef} label='Console output'>
+                        <pre className='RunViewConsole'>
+                            {this.state.console}
+                        </pre>
+                    </Collapsible>
+                }
+            </div>
+        )
+    }
+
+    private getView() {
+        switch(this.state.currentStep) {
+            case Step.Running:
+                return <Spinner label="Running..." />;
+        }
+        return (
+            <div>
+                <div className='DisplayFlex ModelPathBrowser'>
+                    <TextField id='modelToRun' placeholder='Model Path' value={this.state.model || this.props.file && this.props.file.path} label='Model to Run' onChanged={this.setModel} />
                     <DefaultButton id='ConverterModelInputBrowse' text='Browse' onClick={this.browseSource}/>
                 </div>
                 <DefaultButton id='RunButton' text='Run' disabled={!this.state.model} onClick={this.execModelRunner}/>
-
             </div>
         )
     }
@@ -80,15 +113,25 @@ class RunView extends React.Component<IComponentProperties, IComponentState> {
     };
 
     private execModelRunner = async() => {
+        this.setState({
+            console: '',
+            currentStep: Step.Running,
+        });
         const paramters = ['-model', this.state.model];
         try {
             await execFilePromise(modelRunnerPath, paramters, {}, this.outputListener);
         } catch (e) {
             this.printError(e);
+            this.setState({
+                currentStep: Step.Idle,
+            });
             return;
         }
+        this.setState({
+            currentStep: Step.Success,
+        });
         // tslint:disable-next-line:no-console
-        console.log(this.state.console);
+        console.log(this.state.currentStep.toString());
     }
 }
 

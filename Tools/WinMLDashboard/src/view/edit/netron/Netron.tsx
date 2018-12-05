0ccd958e6ccf5ab5ff1080861e856b2bec35c615
xiang zhang <xianz@microsoft.com>
2018-12-05 15:30:17 -0800

    add 'WinMLDashboard' to default page

--- Tools/WinMLDashboard/src/view/edit/netron/Netron.tsx
+++ Tools/WinMLDashboard/src/view/edit/netron/Netron.tsx
@@ -91,6 +91,13 @@ class Netron extends React.Component<IComponentProperties, IComponentState> {
     // }
 
     public render() {
+        const inlineStyle = {
+            color:'lightgray',
+            fontSize:'50px',
+            fontStyle: 'oblique',
+            top: '600px', 
+            width: '400px'
+        }
         return (
             // Instead of hardcoding the page, a div with dangerouslySetInnerHTML could be used to include Netron's content
             <div className='netron-root'>
@@ -101,6 +108,7 @@ class Netron extends React.Component<IComponentProperties, IComponentState> {
                     </div>
                     <button id='open-file-button' className='center' disabled={true}> Open Model...</button>
                     <button id='openFileButton' className='center' style={{top: '200px', width: '100px'}} onClick={onOpen}>Open Model</button>
+                    <p className='center' style={inlineStyle}>WinML Dashboard</p>
                     <input type="file" id="openFileButton" style={{display: 'none'}} multiple={false} accept=".onnx, .pb, .meta, .tflite, .keras, .h5, .json, .mlmodel, .caffemodel" />
                     <div style={{fontWeight: 'normal', color: '#e6e6e6', userSelect: 'none'}}>.</div>
                     <div style={{fontWeight: 600, color: '#e6e6e6', userSelect: 'none'}}>.</div>

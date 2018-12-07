e3a76bdc197a081178c5d69bc90c339226e640bd
Ori Levari <orlevari@microsoft.com>
2018-12-07 13:20:27 -0800

    use argparse in python scripts. add comments

--- Samples/custom-operator-cpu-sample/customize_model/debug_all_outputs.py
+++ Samples/custom-operator-cpu-sample/customize_model/debug_all_outputs.py
@@ -4,26 +4,37 @@ import sys
 import os.path as path
 import onnx
 from onnx import helper
+import argparse
 
 
-if (len(sys.argv) != 4):
-    print('\nusage: `python debug_all_outputs.py <input onnx model path> <output onnx model path> <png|text>\n')
-    sys.exit()
+def create_modified_model(args):
 
-model_path = path.join(path.dirname(path.abspath(__file__)), sys.argv[1])
-modified_path = path.join(path.dirname(path.abspath(__file__)), sys.argv[2])
+    model_path = path.join(path.dirname(path.abspath(__file__)), args.model_path)
+    modified_path = path.join(path.dirname(path.abspath(__file__)), args.modified_path)
+    model = onnx.load(model_path)
 
-model = onnx.load(model_path)
+    intermediate_outputs = set()
 
-intermediate_outputs = set()
+    # gather each unique intermediate output in the graph
+    for node in model.graph.node:
+        for output in node.output:
+            intermediate_outputs.add(output)
 
-for node in model.graph.node:
-    for output in node.output:
-        intermediate_outputs.add(output)
+    # create a debug operator that consumes each intermediate output
+    # debug operator file path attribute is constructed by the name of the intermediate output
+    for output in intermediate_outputs:
+        inserted_node = model.graph.node.add()
+        valid_output_path = output.replace('/', '\\')
+        inserted_node.CopyFrom(helper.make_node('Debug', [output], ['unused_' + output], file_type=args.debug_file_type, file_path=valid_output_path + '\\' + valid_output_path))
 
-for output in intermediate_outputs:
-    inserted_node = model.graph.node.add()
-    valid_output_path = output.replace('/', '-')
-    inserted_node.CopyFrom(helper.make_node('Debug', [output], ['unused_' + output], file_type=sys.argv[3], file_path=valid_output_path + '\\' + valid_output_path))
+    onnx.save(model, modified_path)
 
-onnx.save(model, modified_path)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_path', help='source model path to add debug nodes', required=True)
+    parser.add_argument('--modified_path', help='output model path', required=True)
+    parser.add_argument('--debug_file_type', help='file_type attribute of Debug Operator inserted', choices=['png', 'text'], required=True)
+    args = parser.parse_args()
+    create_modified_model(args)
--- Samples/custom-operator-cpu-sample/customize_model/debug_single_output.py
+++ Samples/custom-operator-cpu-sample/customize_model/debug_single_output.py
@@ -4,20 +4,27 @@ import sys
 import os.path as path
 import onnx
 from onnx import helper
+import argparse
 
 
-if (len(sys.argv) != 5):
-    print('\nusage: `customize_model_debug.py <input onnx model path> <output onnx model path> <png|text> <output name>\n')
-    sys.exit()
+def create_modified_model(args):
+    model_path = path.join(path.dirname(path.abspath(__file__)), args.model_path)
+    modified_path = path.join(path.dirname(path.abspath(__file__)), args.modified_path)
+    model = onnx.load(model_path)
 
-model_path = path.join(path.dirname(path.abspath(__file__)), sys.argv[1])
-modified_path = path.join(path.dirname(path.abspath(__file__)), sys.argv[2])
+    #insert new debug operator node into graph with given attributes and input
+    inserted_node = model.graph.node.add()
+    valid_output_path = args.debug_file_path.replace('/', '\\')
+    inserted_node.CopyFrom(helper.make_node('Debug', [args.intermediate_output], ['unused_' + args.intermediate_output], file_type=args.debug_file_type, file_path=valid_output_path))
 
-model = onnx.load(model_path)
+    onnx.save(model, modified_path)
 
-inserted_node = model.graph.node.add()
-output = sys.argv[4]
-valid_output_path = output.replace('/', '-')
-inserted_node.CopyFrom(helper.make_node('Debug', [output], ['unused_' + output], file_type=sys.argv[3], file_path=valid_output_path + '\\' + valid_output_path))
-
-onnx.save(model, modified_path)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_path', help='source model path to add debug nodes', required=True)
+    parser.add_argument('--modified_path', help='output model path', required=True)
+    parser.add_argument('--debug_file_type', help='file_type attribute of Debug Operator inserted', choices=['png', 'text'], required=True)
+    parser.add_argument('--debug_file_path', help='file_path attribute of Debug Operator inserted', required=True)
+    parser.add_argument('--intermediate_output', help='the intermediate output this Debug Operator will consume', required=True)
+    args = parser.parse_args()
+    create_modified_model(args)

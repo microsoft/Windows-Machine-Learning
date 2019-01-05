be939fd4d6cf5701711672d94abe77e4390a480e
Ryan Lai <rylai@microsoft.com>
2019-01-04 16:01:26 -0800

    Fixed release build code generation linking

--- Tools/WinMLRunner/src/dllmain.cpp
+++ Tools/WinMLRunner/src/dllmain.cpp
@@ -1,10 +1,17 @@
 #include <Windows.h>
 
-BOOL WINAPI DllMain(
-    _In_ HINSTANCE hinstDLL,
-    _In_ DWORD     fdwReason,
-    _In_ LPVOID    lpvReserved
+BOOL APIENTRY DllMain(HMODULE hModule,
+    DWORD  ul_reason_for_call,
+    LPVOID lpReserved
 )
 {
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
     return TRUE;
 }

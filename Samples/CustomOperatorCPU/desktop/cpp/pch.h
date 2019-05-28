d938b31321e8a6b398a7e36bc26e63841f820e6d
Ori Levari <orlevari@microsoft.com>
2019-05-28 14:41:12 -0700

    add include statement for windows.foundation.collection.h due to change in newer windows sdk

--- Samples/CustomOperatorCPU/desktop/cpp/pch.h
+++ Samples/CustomOperatorCPU/desktop/cpp/pch.h
@@ -14,6 +14,7 @@
 #include <winstring.h>
 
 #include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
 #include <winrt/Windows.AI.MachineLearning.h>
 #include <winrt/Windows.Media.h>
 #include <winrt/Windows.Storage.h>
--- Samples/SqueezeNetObjectDetection/Desktop/cpp/pch.h
+++ Samples/SqueezeNetObjectDetection/Desktop/cpp/pch.h
@@ -12,6 +12,7 @@
 #include <windows.h>
 
 #include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
 #include <winrt/Windows.AI.MachineLearning.h>
 #include <winrt/Windows.Media.h>
 #include <winrt/Windows.Storage.h>

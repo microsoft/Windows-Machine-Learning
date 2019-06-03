c6cc8c63c3d32e0cbda9c2c37423ec5ceb786267
Brian Martin <42186431+martinb35@users.noreply.github.com>
2019-06-03 14:17:19 -0700

    User/brian/steamfromresource (#239)
    * sample loading a model from an embedded resource
    
    * add to build pipeline
    
    * put it all in a try/catch and free the resource earlier
    
    * remove unnecessary seek
    
    * remove commented out code
    
    * add an accurate readme

---
+++ Samples/StreamFromResource/StreamFromResource/main.cpp
@@ -0,0 +1,74 @@
+﻿#include "pch.h"
+#include <Windows.h>
+#include "resource.h"
+#include <winrt/windows.storage.streams.h>
+#include <winrt/Windows.AI.MachineLearning.h>
+
+using namespace winrt;
+using namespace winrt::Windows::Storage::Streams;
+using namespace winrt::Windows::AI::MachineLearning;
+
+int main()
+{
+    init_apartment();
+
+    // Find the resource
+    const auto modelResource = FindResource(NULL, MAKEINTRESOURCE(IDR_SQUEEZENET_MODEL), DataFileTypeString);
+    if (modelResource == nullptr)
+    {
+        printf("failed to find resource.");
+        return 1;
+    }
+
+    // Load the resource
+    const auto modelMem = LoadResource(NULL, modelResource);
+    if (modelMem == nullptr)
+    {
+        printf("failed to load resource");
+        return 2;
+    }
+
+    try
+    {
+        // get a byte point to the resource
+        const unsigned char* pModelData = static_cast<const unsigned char*>(LockResource(modelMem));
+        const auto size = SizeofResource(NULL, modelResource);
+
+        // write the bytes into a stream
+        InMemoryRandomAccessStream modelStream;
+        DataWriter writer(modelStream);
+        writer.WriteBytes(array_view<const unsigned char>(pModelData, pModelData + size));
+        writer.StoreAsync().get();
+
+        // clean up.
+        FreeResource(modelMem);
+
+        // wrap the stream in a stream reference
+        auto modelStreamReference = RandomAccessStreamReference::CreateFromStream(modelStream);
+
+        // load the model from stream reference
+        auto learningModel = LearningModel::LoadFromStream(modelStreamReference);
+
+        // create the session binding
+        auto learningModelSession = LearningModelSession(learningModel);
+        auto learningModelBinding = LearningModelBinding(learningModelSession);
+
+        // bind uninitialized data
+        std::vector<int64_t> shape = { 1,3,224,224 };
+        std::vector<float> inputData;
+        inputData.resize(3 * 224 * 224);
+        auto inputTensor = TensorFloat::CreateFromArray(shape, inputData);
+        learningModelBinding.Bind(L"data_0", inputTensor);
+
+        // evaluate
+        auto results = learningModelSession.Evaluate(learningModelBinding, L"");
+    }
+    catch (...)
+    {
+        printf("caught an exception");
+        return 3;
+    }
+
+    printf("success!");
+    return 0;
+}
---
+++ Samples/StreamFromResource/StreamFromResource/resource.h
@@ -0,0 +1,19 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Resource.rc
+
+// Next default values for new objects
+// 
+
+#define DataFileType        DataFileResource
+#define DataFileTypeString  L"DataFileResource"
+#define IDR_SQUEEZENET_MODEL 101
+
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif

de1a05ea65343b392c3870a8ef0e26c81cb4fd08
linnealovespie <linneamay98@gmail.com>
2020-06-02 19:33:13 -0400

    skeleton codebase

--- Samples/StyleTransfer/App.xaml.cs
+++ Samples/StyleTransfer/App.xaml.cs
@@ -15,7 +15,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-namespace VideoStreamDemo
+namespace StyleTransfer
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
--- Samples/StyleTransfer/MainPage.xaml.cs
+++ Samples/StyleTransfer/MainPage.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -13,10 +12,11 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Microsoft.AI.MachineLearning;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace VideoStreamDemo
+namespace StyleTransfer
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
@@ -26,32 +26,6 @@ namespace VideoStreamDemo
         public MainPage()
         {
             this.InitializeComponent();
-            this.ViewModel = new PageViewModel();
         }
-
-        // Hacky version of button click- not currently hooked up to the media buttons, though. 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            if (sender is Button b)
-            {
-                string name = b.Name;
-                switch (name)
-                {
-                    case "UIButtonLiveStream":
-                        ViewModel.SelectedSource = "LiveStream";
-                        break;
-                    case "UIButtonAcquireImage":
-                        ViewModel.SelectedSource = "AcquireImage";
-                        break;
-                    case "UIButtonFilePick":
-                        ViewModel.SelectedSource = "FilePick";
-                        break;
-                    case "UIButtonInking":
-                        ViewModel.SelectedSource = "Inking";
-                        break;
-                }
-            }
-        }
-        public PageViewModel ViewModel { get; set; }
     }
 }

eff0aaeab30e286f8e8bf31b89dd08f034500fd6
linnealovespie <linneamay98@gmail.com>
2020-06-02 19:17:44 -0400

    connecting buttons to ViewModel and trying to pass commands from media selection buttons

---
+++ Samples/StyleTransfer/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace VideoStreamDemo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.ViewModel = new PageViewModel();
+        }
+
+        // Hacky version of button click- not currently hooked up to the media buttons, though. 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button b)
+            {
+                string name = b.Name;
+                switch (name)
+                {
+                    case "UIButtonLiveStream":
+                        ViewModel.SelectedSource = "LiveStream";
+                        break;
+                    case "UIButtonAcquireImage":
+                        ViewModel.SelectedSource = "AcquireImage";
+                        break;
+                    case "UIButtonFilePick":
+                        ViewModel.SelectedSource = "FilePick";
+                        break;
+                    case "UIButtonInking":
+                        ViewModel.SelectedSource = "Inking";
+                        break;
+                }
+            }
+        }
+        public PageViewModel ViewModel { get; set; }
+    }
+}
---
+++ Samples/StyleTransfer/PageViewModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace VideoStreamDemo
+{
+
+    public class PageViewModel : INotifyPropertyChanged
+    {
+
+        public List<string> modelFileNames = new List<string>
+            {"candy",
+                "mosaic",
+                "la_muse",
+                "udnie"
+            };
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public PageViewModel()
+        {
+            useGpu = true;
+            selectedModel = "candy";
+        }
+
+        bool useGpu; // Determines if should use GPU. Default to True.
+        public bool InferenceDevice
+        {
+            set
+            {
+                if (useGpu != value)
+                {
+                    useGpu = value;
+                    OnPropertyChanged("InferenceDevice");
+                }
+            }
+            get
+            {
+                return useGpu;
+            }
+        }
+
+        private string selectedModel;
+        public string SelectedModel
+        {
+            get { return selectedModel; }
+            set
+            {
+                if (value != this.selectedModel)
+                {
+                    selectedModel = value;
+                    OnPropertyChanged("SelectedModel");
+                }
+            }
+        }
+
+        private string selectedSource;
+        public string SelectedSource { get; set; }
+
+        // Trying to figure out how to send command for selecting media source
+        /*public ICommand SelectSourceCommand
+        {
+            get
+            {
+                return new Command<string>((x) => selectedSource = x);
+            }
+        }*/
+
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}

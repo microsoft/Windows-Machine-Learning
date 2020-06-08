940024cf405ea134d98a3426c142adcbcd09595b
linnealovespie <linneamay98@gmail.com>
2020-06-08 16:04:39 -0400

    added MVVMLight framework for better RelayCommand implementation

--- Samples/StyleTransfer/AppViewModel.cs
+++ Samples/StyleTransfer/AppViewModel.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Windows.Media.Core;
+using GalaSoft.MvvmLight.Command;
 
 namespace StyleTransfer
 {
@@ -33,16 +34,18 @@ namespace StyleTransfer
         public AppViewModel()
         {
             _appModel = new AppModel();
-            SaveCommand = new RelayCommand(new Action<object>(SaveOutput));
-            MediaSourceCommand = new RelayCommand(new Action<object>(SetMediaSource));
+            // TODO: Add a param to check if have an image to save
+            SaveCommand = new RelayCommand(this.SaveOutput);
+            MediaSourceCommand = new RelayCommand<string>(SetMediaSource);
         }
 
-        public void SaveOutput(object obj)
+        public void SaveOutput()
         {
             return;
         }
         public void SetMediaSource(object obj)
         {
+            // TODO: Convert to a better value for the appModel object here 
             _appModel.InputMediaSource = obj.ToString();
             return;
         }

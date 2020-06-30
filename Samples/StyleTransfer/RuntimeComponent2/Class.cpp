b9f25c62c826be515ea45a3d39387f54e5c661b0
linnealovespie <linneamay98@gmail.com>
2020-06-30 18:43:49 -0400

    start of cpp version

---
+++ Samples/StyleTransfer/RuntimeComponent2/Class.cpp
@@ -0,0 +1,91 @@
+﻿#include "pch.h"
+#include "Class.h"
+#include "Class.g.cpp"
+
+
+namespace winrt::RuntimeComponent2::implementation
+{
+	int32_t Class::MyProperty()
+	{
+		throw hresult_not_implemented();
+	}
+
+	void Class::MyProperty(int32_t /* value */)
+	{
+		throw hresult_not_implemented();
+	}
+
+	IVectorView<VideoEncodingProperties> Class::SupportedEncodingProperties() {
+		VideoEncodingProperties encodingProperties = VideoEncodingProperties();
+		encodingProperties.Subtype(L"ARGB32");
+		return single_threaded_vector(std::move(std::vector<VideoEncodingProperties>{encodingProperties})).GetView();
+	}
+
+	LearningModelSession Class::Session() {
+		if (!configuration) {
+			IInspectable val = configuration.TryLookup(L"Session");
+			if (val) {
+				return val.try_as<LearningModelSession>();
+			}
+		}
+		return NULL;
+	}
+	LearningModelBinding Class::Binding() {
+		if (!configuration) {
+			IInspectable val = configuration.TryLookup(L"Binding");
+			if (!configuration && val) {
+				return val.try_as<LearningModelBinding>();
+			}
+		}
+		return NULL;
+	}
+
+	hstring Class::InputImageDescription() {
+		if (!configuration) {
+			IInspectable val = configuration.TryLookup(L"InputImageDescription");
+			if (!configuration && val) {
+				return unbox_value<hstring>(val);
+			}
+		}
+		return L"";
+	}
+	hstring Class::OutputImageDescription() {
+		if (!configuration) {
+			IInspectable val = configuration.TryLookup(L"OutputImageDescription");
+			if (!configuration && val) {
+				return unbox_value<hstring>(val);
+			}
+		}
+		return L"";
+	}
+
+
+	bool Class::TimeIndependent() { return true; }
+	MediaMemoryTypes Class::SupportedMemoryTypes() { return MediaMemoryTypes::GpuAndCpu; }
+	bool Class::IsReadOnly() { return false; }
+	void Class::DiscardQueuedFrames() {}
+
+	void Class::Close(MediaEffectClosedReason) {
+		throw hresult_not_implemented();
+	}
+
+	void Class::ProcessFrame(ProcessVideoFrameContext context) {
+		LearningModelSession _session = Session();
+		LearningModelBinding _binding = Binding();
+		VideoFrame inputFrame = context.InputFrame();
+		VideoFrame outputFrame = context.OutputFrame();
+
+		_binding.Bind(InputImageDescription(), inputFrame);
+		_binding.Bind(OutputImageDescription(), outputFrame);
+
+		auto results = _session.Evaluate(_binding, L"test");
+	}
+
+	void Class::SetEncodingProperties(VideoEncodingProperties, IDirect3DDevice) {
+		throw hresult_not_implemented();
+	}
+
+	void Class::SetProperties(IPropertySet config) {
+		configuration = config;
+	}
+}
---
+++ Samples/StyleTransfer/RuntimeComponent2/Class.h
@@ -0,0 +1,47 @@
+﻿#pragma once
+
+#include "Class.g.h"
+#include "winrt\Windows.Media.Effects.h"
+
+using namespace winrt::Windows::Media::Effects;
+using namespace winrt::Windows::Media::MediaProperties;
+using namespace winrt::Windows::Graphics::DirectX::Direct3D11;
+using namespace winrt::Windows::Foundation::Collections;
+using namespace winrt::Microsoft::AI::MachineLearning;
+using namespace winrt::Windows::Media;
+
+namespace winrt::RuntimeComponent2::implementation
+{
+	struct Class : ClassT<Class>
+	{
+		Class() = default;
+
+		int32_t MyProperty();
+		IVectorView<VideoEncodingProperties> SupportedEncodingProperties();
+		bool TimeIndependent();
+		MediaMemoryTypes SupportedMemoryTypes();
+		bool IsReadOnly();
+		IPropertySet configuration;
+
+		void MyProperty(int32_t value);
+		void DiscardQueuedFrames();
+		void Close(MediaEffectClosedReason);
+		void ProcessFrame(ProcessVideoFrameContext);
+		void SetEncodingProperties(VideoEncodingProperties, IDirect3DDevice);
+		void SetProperties(IPropertySet);
+
+	private:
+		LearningModelSession Session();
+		LearningModelBinding Binding();
+		hstring InputImageDescription();
+		hstring OutputImageDescription();
+
+	};
+}
+
+namespace winrt::RuntimeComponent2::factory_implementation
+{
+	struct Class : ClassT<Class, implementation::Class>
+	{
+	};
+}

b2b3c5a7bdba7d6134e19d891ed21dcd81036a57
Ori Levari <orlevari@microsoft.com>
2020-08-05 14:47:51 -0700

    add named dimension override sample using fns-candy.

---
+++ Samples/NamedDimensionOverrides/NamedDimensionOverrides/pch.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "winrt/Microsoft.AI.MachineLearning.h"
+#include "winrt/Windows.Foundation.h"
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.Graphics.Imaging.h>
+#include <winrt/Windows.Media.h>
+#include "FileHelper.h"

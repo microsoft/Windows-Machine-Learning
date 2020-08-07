18ef6643c160c077b3ee092c7cd155230663ba27
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2020-08-07 10:04:31 -0700

    Add MNIST Redist UWP sample

---
+++ Samples/MNISTRedist/cs/mnist.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AI.MachineLearning;
+using Windows.Media;
+using Windows.Storage;
+using Windows.Storage.Streams;
+namespace MNIST_Demo
+{
+
+    public sealed class mnistInput
+    {
+        public ImageFeatureValue Input3; // BitmapPixelFormat: Gray8, BitmapAlphaMode: Premultiplied, width: 28, height: 28
+    }
+
+    public sealed class mnistOutput
+    {
+        public TensorFloat Plus214_Output_0; // shape(1,10)
+    }
+
+    public sealed class mnistModel
+    {
+        private LearningModel model;
+        private LearningModelSession session;
+        private LearningModelBinding binding;
+        public static async Task<mnistModel> CreateFromStreamAsync(IRandomAccessStreamReference stream)
+        {
+            mnistModel learningModel = new mnistModel();
+            learningModel.model = await LearningModel.LoadFromStreamAsync(stream);
+            learningModel.session = new LearningModelSession(learningModel.model);
+            learningModel.binding = new LearningModelBinding(learningModel.session);
+            return learningModel;
+        }
+        public async Task<mnistOutput> EvaluateAsync(mnistInput input)
+        {
+            binding.Bind("Input3", input.Input3);
+            var result = await session.EvaluateAsync(binding, "0");
+            var output = new mnistOutput();
+            output.Plus214_Output_0 = result.Outputs["Plus214_Output_0"] as TensorFloat;
+            return output;
+        }
+    }
+}

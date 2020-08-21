bb9c9d5d4db689ebfc0e91e06cbca4e8566eeabe
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2020-08-20 23:34:10 -0700

    Workaround winrt sdk bug
    https://github.com/microsoft/Windows.UI.Composition-Win32-Samples/issues/47#issuecomment-672696574

--- Samples/SampleSharedLib/SampleSharedLib/pch.h
+++ Samples/SampleSharedLib/SampleSharedLib/pch.h
@@ -1,9 +1,18 @@
 #ifndef PCH_H
 #define PCH_H
 
+#include <winrt/base.h>
+namespace winrt::impl
+{
+    template <typename Async>
+    auto wait_for(Async const& async, Windows::Foundation::TimeSpan const& timeout);
+}
+
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Graphics.h>
 #include <winrt/Windows.Graphics.Imaging.h>
 #include <winrt/Windows.Storage.h>
-#include "winrt/Windows.Storage.Streams.h"
+#include <winrt/Windows.Storage.Streams.h>
 
 #include <Windows.h>
 

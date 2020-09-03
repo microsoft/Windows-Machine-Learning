9fe99f7d5cad00295c1c2bc8c1705572a2e0a93c
Ryan Lai <ryalai96@gamil.com>
2020-09-03 15:24:00 -0700

    Remove windows.ai.machinelearning header

--- Tools/WinMLRunner/src/Common.h
+++ Tools/WinMLRunner/src/Common.h
@@ -4,7 +4,6 @@
 #endif
 // unknown.h needs to be inlcuded before any winrt headers
 #include <unknwn.h>
-//#include <winrt/Windows.AI.MachineLearning.h>
 #include <winrt/Microsoft.AI.MachineLearning.h>
 #include <winrt/Windows.Foundation.Collections.h>
 #include <winrt/Windows.Media.h>

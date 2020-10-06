199318931b1a568e8fab594a83955e655f0a343a
Ori Levari <orlevari@microsoft.com>
2020-10-06 11:13:41 -0700

    Revert "add named dimension override sample using fns-candy."
    This reverts commit b2b3c5a7bdba7d6134e19d891ed21dcd81036a57.

--- Samples/NamedDimensionOverrides/NamedDimensionOverrides/pch.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "winrt/Microsoft.AI.MachineLearning.h"
-#include "winrt/Windows.Foundation.h"
-#include <winrt/Windows.Foundation.Collections.h>
-#include <winrt/Windows.Graphics.Imaging.h>
-#include <winrt/Windows.Media.h>
-#include "FileHelper.h"

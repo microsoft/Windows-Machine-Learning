4cdfc75c9d5daa4307d633881b957c33fa932ff0
QuinnRadich <quradic@microsoft.com>
2021-04-29 13:16:46 -0700

    add pytorch image classification tutorial, final state

---
+++ Samples/Tutorial Samples/classifierPyTorchModel/Helper.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Threading.Tasks;
+using Windows.Graphics.Imaging;
+using Windows.Media;
+
+namespace classifierPyTorch
+{
+    public class Helper
+    {
+        private const int SIZE = 32; 
+        VideoFrame cropped_vf = null;
+        public async Task<VideoFrame> CropAndDisplayInputImageAsync(VideoFrame inputVideoFrame)
+        {
+            bool useDX = inputVideoFrame.SoftwareBitmap == null;
+
+            BitmapBounds cropBounds = new BitmapBounds();
+            uint h = SIZE;
+            uint w = SIZE;
+            var frameHeight = useDX ? inputVideoFrame.Direct3DSurface.Description.Height : inputVideoFrame.SoftwareBitmap.PixelHeight;
+            var frameWidth = useDX ? inputVideoFrame.Direct3DSurface.Description.Width : inputVideoFrame.SoftwareBitmap.PixelWidth;
+
+            var requiredAR = ((float)SIZE / SIZE);
+            w = Math.Min((uint)(requiredAR * frameHeight), (uint)frameWidth);
+            h = Math.Min((uint)(frameWidth / requiredAR), (uint)frameHeight);
+            cropBounds.X = (uint)((frameWidth - w) / 2);
+            cropBounds.Y = 0;
+            cropBounds.Width = w;
+            cropBounds.Height = h;
+
+            cropped_vf = new VideoFrame(BitmapPixelFormat.Bgra8, SIZE, SIZE, BitmapAlphaMode.Ignore);
+
+            await inputVideoFrame.CopyToAsync(cropped_vf, cropBounds, null);
+            return cropped_vf;
+        }
+    }
+}
---
+++ Samples/Tutorial Samples/classifierPyTorchModel/MainPage.xaml.cs
@@ -0,0 +1,175 @@
+﻿//Specify all the using statements which give us the access to all the APIs that we'll need
+using System;
+using System.Threading.Tasks;
+using Windows.AI.MachineLearning;
+using Windows.Graphics.Imaging;
+using Windows.Media;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+
+
+namespace classifierPyTorch
+{
+    public sealed partial class MainPage : Page
+    {
+        // All the required fields declaration
+        private ImageClassifierModel modelGen;
+        private ImageClassifierInput image = new ImageClassifierInput();
+        private ImageClassifierOutput results;
+        private StorageFile selectedStorageFile;
+        private string label = "";
+        private float probability = 0;
+        private Helper helper = new Helper();
+
+        public enum Labels
+        {            
+            plane,
+            car,
+            bird,
+            cat,
+            deer,
+            dog,
+            frog,
+            horse,
+            ship,
+            truck
+        }
+
+        // The main page to initialize and execute the model.
+        public MainPage()
+        {
+            this.InitializeComponent();
+            loadModel();
+        }
+
+        // A method to load a machine learning model.
+        private async Task loadModel()
+        {
+            // Get an access the ONNX model and save it in memory. 
+            StorageFile modelFile = await StorageFile.GetFileFromApplicationUriAsync(new Uri($"ms-appx:///Assets/ONNX.onnx"));
+            // Instantiate the model. 
+            modelGen = await ImageClassifierModel.CreateFromStreamAsync(modelFile);
+        }
+
+        // Waiting for a click event to select a file 
+        private async void OpenFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!await getImage())
+            {
+                return;
+            }
+            // After the click event happened and an input selected, we begin the model execution. 
+            // Bind the model input
+            await imageBind();
+            // Model evaluation
+            await evaluate();
+            // Extract the results
+            extractResult();
+            // Display the results  
+            await displayResult();
+        }
+
+        // A method to select an input image file
+        private async Task<bool> getImage()
+        {
+            try
+            {
+                // Trigger file picker to select an image file
+                FileOpenPicker fileOpenPicker = new FileOpenPicker();
+                fileOpenPicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+                fileOpenPicker.FileTypeFilter.Add(".jpg");
+                fileOpenPicker.FileTypeFilter.Add(".png");
+                fileOpenPicker.ViewMode = PickerViewMode.Thumbnail;
+                selectedStorageFile = await fileOpenPicker.PickSingleFileAsync();
+                if (selectedStorageFile == null)
+                {
+                    return false;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        // A method to convert and bind the input image.   
+        private async Task imageBind()
+        {
+            UIPreviewImage.Source = null;
+
+            try
+            {
+                SoftwareBitmap softwareBitmap;
+
+                using (IRandomAccessStream stream = await selectedStorageFile.OpenAsync(FileAccessMode.Read))
+                {
+                    
+                    // Create the decoder from the stream 
+                    BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
+
+                    // Get the SoftwareBitmap representation of the file in BGRA8 format
+                    softwareBitmap = await decoder.GetSoftwareBitmapAsync();
+                    softwareBitmap = SoftwareBitmap.Convert(softwareBitmap, BitmapPixelFormat.Bgra8, BitmapAlphaMode.Premultiplied);
+                }
+                
+                // Display the image
+                SoftwareBitmapSource imageSource = new SoftwareBitmapSource();
+                await imageSource.SetBitmapAsync(softwareBitmap);
+                UIPreviewImage.Source = imageSource;
+
+                // Encapsulate the image within a VideoFrame to be bound and evaluated
+                VideoFrame inputImage = VideoFrame.CreateWithSoftwareBitmap(softwareBitmap);
+                // Resize the image size to 32x32 using the Helper we have defined earlier.
+                inputImage=await helper.CropAndDisplayInputImageAsync(inputImage);
+                // Bind the model input
+                ImageFeatureValue imageTensor = ImageFeatureValue.CreateFromVideoFrame(inputImage);
+                image.modelInput = imageTensor;
+            }
+            catch (Exception e)
+            {
+            }
+        }
+
+        // A method to evaluate the model
+        private async Task evaluate()
+        {
+            results = await modelGen.EvaluateAsync(image);
+        }
+
+        // A method to extract the output from the the model 
+        private void extractResult()
+        {
+            // Retrieve the results of evaluation
+            var mResult = results.modelOutput as TensorFloat;
+            // Convert the result to vector format
+            var resultVector = mResult.GetAsVectorView();
+
+            probability = 0;
+            int index = 0;
+            // find the maximum "energy" of the label
+            for (int i = 0; i < resultVector.Count; i++)
+            {
+                var elementProbability = resultVector[i];
+
+                if (elementProbability > probability)
+                {
+                    index = i;
+                }
+                System.Diagnostics.Debug.WriteLine(i+" "+ elementProbability);
+            }
+            label = ((Labels)index).ToString();
+        }
+
+        // A method to display the result
+        private async Task displayResult()
+        {
+            displayOutput.Text = label;
+        }
+    }
+
+}

ac4941a62d3f2435cdc43668cea1433052c3637e
janezdu <zd53@cornell.edu>
2021-06-15 16:02:42 -0400

    Hello world mvvm

---
+++ Samples/AudioPreprocessing/AudioPreprocessing/Model/MelSpectrogramModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioPreprocessing.Model
+{
+    public class MelSpectrogramModel
+    {
+        private List<string> repositoryData;
+        public string ImportantInfo
+        {
+            get
+            {
+                return ConcatenateData(repositoryData);
+            }
+        }
+
+        public MelSpectrogramModel()
+        {
+            repositoryData = GetData();
+        }
+
+        /// <summary>
+        /// Simulates data retrieval from a repository
+        /// </summary>
+        /// <returns>List of strings</returns>
+        private List<string> GetData()
+        {
+            repositoryData = new List<string>()
+            {
+                "Hello",
+                "world"
+            };
+            return repositoryData;
+        }
+
+        /// <summary>
+        /// Concatenate the information from the list into a fully formed sentence.
+        /// </summary>
+        /// <returns>A string</returns>
+        private string ConcatenateData(List<string> dataList)
+        {
+            string importantInfo = dataList.ElementAt(0) + ", " + dataList.ElementAt(1) + "!";
+            return importantInfo;
+        }
+    }
+}
---
+++ Samples/AudioPreprocessing/AudioPreprocessing/ViewModel/MelSpectrogramViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using AudioPreprocessing.Model;
+
+namespace AudioPreprocessing.ViewModel
+{
+    // Implements INotifyPropertyChanged interface to support bindings
+    public class MelSpectrogramViewModel : INotifyPropertyChanged
+    {
+        private string helloString;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string HelloString
+        {
+            get
+            {
+                return helloString;
+            }
+            set
+            {
+                helloString = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Raises OnPropertychangedEvent when property changes
+        /// </summary>
+        /// <param name="name">String representing the property name</param>
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public MelSpectrogramViewModel()
+        {
+            MelSpectrogramModel helloWorldModel = new MelSpectrogramModel();
+            helloString = helloWorldModel.ImportantInfo;
+        }
+    }
+
+}
+

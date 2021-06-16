b441802fc92e605e63356f8c914aacfde3daf75b
janezdu <zd53@cornell.edu>
2021-06-16 17:12:04 -0400

    Clean up commented code

--- Samples/AudioPreprocessing/AudioPreprocessing/Model/PreprocessModel.cs
+++ Samples/AudioPreprocessing/AudioPreprocessing/Model/PreprocessModel.cs
@@ -9,54 +9,8 @@ namespace AudioPreprocessing.Model
 {
     public class PreprocessModel
     { 
-        private List<string> repositoryData;
         private string melSpecPath;
-        public string ImportantInfo
-        {
-            get
-            {
-                return ConcatenateData(repositoryData);
-            }
-        }
-        public string MelSpecPath
-        {
-            get
-            {
-                return melSpecPath;
-            }
-        }
-
-        public PreprocessModel()
-        {
-            repositoryData = GetData();
-            melSpecPath = "C:/Users/t-janedu/source/repos/Windows-Machine-Learning/Samples/AudioPreprocessing/AudioPreprocessing/tmp/mel_spectrogram_file.jpg";
-        }
-
-        /// <summary>
-        /// Simulates data retrieval from a repository
-        /// </summary>
-        /// <returns>List of strings</returns>
-        private List<string> GetData()
-        {
-            repositoryData = new List<string>()
-            {
-                "Hello",
-                "world"
-            };
-            return repositoryData;
-        }
-
-        /// <summary>
-        /// Concatenate the information from the list into a fully formed sentence.
-        /// </summary>
-        /// <returns>A string</returns>
-        private string ConcatenateData(List<string> dataList)
-        {
-            string importantInfo = dataList.ElementAt(0) + ", " + dataList.ElementAt(1) + "!";
-            return importantInfo;
-        }
-
-
-
+ 
+        public string MelSpecPath { get; set; }
     }
 }
--- Samples/AudioPreprocessing/AudioPreprocessing/ViewModel/PreprocessViewModel.cs
+++ Samples/AudioPreprocessing/AudioPreprocessing/ViewModel/PreprocessViewModel.cs
@@ -7,26 +7,20 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using AudioPreprocessing.Model;
-using Microsoft.Toolkit.Mvvm;
-using Microsoft.Toolkit.Mvvm.Input;
-using MvvmDialogs;
-using MvvmDialogs.FrameworkDialogs.OpenFile;
+
 
 namespace AudioPreprocessing.ViewModel
 {
     // Implements INotifyPropertyChanged interface to support bindings
     public class PreprocessViewModel : INotifyPropertyChanged
+    
     {
-        private readonly IDialogService dialogService;
-        private string helloString;
         private string melSpecPath;
 
         public PreprocessViewModel()
         {
             PreprocessModel helloWorldModel = new PreprocessModel();
             melSpecPath = helloWorldModel.MelSpecPath;
-            helloString = helloWorldModel.ImportantInfo;
-            OpenFileCommand = new RelayCommand(OpenFile);
         }
 
         public string MelSpecPath
@@ -39,29 +33,11 @@ namespace AudioPreprocessing.ViewModel
 
         private void OpenFile()
         {
-         
-            var settings = new OpenFileDialogSettings
-            {
-                Title = "This Is The Title",
-                //InitialDirectory = IOPath.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
-                Filter = "Text Documents (*.txt)|*.txt|All Files (*.*)|*.*"
-            };
-
-            bool? success = dialogService.ShowOpenFileDialog(this, settings);
-            if (success == true)
-            {
-                melSpecPath = settings.FileName;
-            }
+            //Logic for opening files
+            Console.WriteLine("Open File");
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
-
-        public string HelloString
-        {
-            get { return helloString; }
-            set { helloString = value; OnPropertyChanged(); }
-        }
-
 
         /// <summary>
         /// Raises OnPropertychangedEvent when property changes

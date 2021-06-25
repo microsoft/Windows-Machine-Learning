f3de7c87ef6442014fb9d9a766b91a2b38d8f530
janezdu <zd53@cornell.edu>
2021-06-25 15:23:04 -0400

    Switch to WinUI

---
+++ Samples/AudioPreprocessing/WinUI/AudioPreprocessing/AudioPreprocessing/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using AudioPreprocessing.ViewModel;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace AudioPreprocessing
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            this.ViewModel = new PreprocessViewModel();
+        }
+
+        public PreprocessViewModel ViewModel { get; set; }
+
+    }
+}
---
+++ Samples/AudioPreprocessing/WinUI/AudioPreprocessing/AudioPreprocessing/ViewModel/PreprocessViewModel.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+using AudioPreprocessing.Model;
+using Microsoft.UI.Xaml.Media.Imaging;
+using Windows.Graphics.Imaging;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using Windows.UI.Core;
+
+namespace AudioPreprocessing.ViewModel
+{
+    // Implements INotifyPropertyChanged interface to support bindings
+    public class PreprocessViewModel : INotifyPropertyChanged
+
+    {
+        private string audioPath;
+        private string imagePath;
+        private string test;
+        private SoftwareBitmapSource melSpectrogramImageSource;
+        private SoftwareBitmap melSpectrogramImage;
+
+        public string Test
+        {
+            get { return test; }
+            set { test = value; OnPropertyChanged(); }
+        }
+        public PreprocessViewModel()
+        {
+            PreprocessModel preprocessModel = new PreprocessModel();
+            audioPath = preprocessModel.AudioPath;
+            imagePath = preprocessModel.MelSpecImagePath;
+            test = "Hello World";
+            //melSpectrogramImage = GetSampleBitmap().Result;
+            MelSpectrogramImageSource = new SoftwareBitmapSource();
+            //MelSpectrogramImageSource.SetBitmapAsync(melSpectrogramImage).GetResults();
+        }
+
+        public async Task<SoftwareBitmap> GetSampleBitmap()
+        {
+            SoftwareBitmap softwareBitmap;
+            FileOpenPicker fileOpenPicker = new FileOpenPicker();
+            fileOpenPicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+            fileOpenPicker.FileTypeFilter.Add(".jpg");
+            fileOpenPicker.ViewMode = PickerViewMode.Thumbnail;
+
+            var inputFile = await fileOpenPicker.PickSingleFileAsync();
+
+            if (inputFile == null)
+            {
+                // The user cancelled the picking operation
+                return null;
+            }
+            using (IRandomAccessStream stream = await inputFile.OpenAsync(FileAccessMode.Read))
+            {
+                // Create the decoder from the stream
+                BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
+
+                // Get the SoftwareBitmap representation of the file
+                softwareBitmap = await decoder.GetSoftwareBitmapAsync();
+            }
+            return softwareBitmap;
+        }
+
+        public string AudioPath
+        {
+            get { return audioPath; }
+            set { audioPath = value; OnPropertyChanged(); }
+        }
+        public string ImagePath
+        {
+            get { return imagePath; }
+            set { imagePath = value; OnPropertyChanged(); }
+        }
+
+        public SoftwareBitmapSource MelSpectrogramImageSource
+        {
+            get { return melSpectrogramImageSource; }
+            set { melSpectrogramImageSource = value; OnPropertyChanged(); }
+        }
+
+
+        public ICommand OpenFileCommand => new AsyncRelayCommand(p => OpenFile());
+
+        private async Task OpenFile()
+        {
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Choose an Audio File";
+            openFileDialog.Filter = "sound files (*.wav)|*.wav|All files (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //PreprocessModel melSpec = new PreprocessModel();
+                //AudioPath = openFileDialog.FileName;
+                //    melSpectrogramImage = melSpec.GenerateMelSpectrogram(AudioPath);
+
+                //    //Microsoft.UI.Xaml.Window.Current.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, 
+                //    //    async() => { await MelSpectrogramImageSource.SetBitmapAsync(melSpectrogramImage); }
+                //    //);
+                //    //await MelSpectrogramImageSource.SetBitmapAsync(melSpectrogramImage);
+            }
+            return;
+        }
+
+        public ICommand SaveFileCommand => new AsyncRelayCommand(p => SaveFile());
+
+        private async Task SaveFile()
+        {
+            FileSavePicker fileSavePicker = new FileSavePicker();
+            fileSavePicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+            fileSavePicker.FileTypeChoices.Add("JPEG files", new List<string>() { ".jpg" });
+            fileSavePicker.SuggestedFileName = "image";
+
+            var outputFile = await fileSavePicker.PickSaveFileAsync();
+
+            if (outputFile == null)
+            {
+                // The user cancelled the picking operation
+                return;
+            }
+            else { SaveSoftwareBitmapToFile(melSpectrogramImage, outputFile); }
+
+
+        }
+
+        private async void SaveSoftwareBitmapToFile(SoftwareBitmap softwareBitmap, StorageFile outputFile)
+        {
+            using (IRandomAccessStream stream = await outputFile.OpenAsync(FileAccessMode.ReadWrite))
+            {
+                // Create an encoder with the desired format
+                BitmapEncoder encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.JpegEncoderId, stream);
+
+                // Set the software bitmap
+                encoder.SetSoftwareBitmap(softwareBitmap);
+
+                // Set additional encoding parameters, if needed
+                encoder.BitmapTransform.ScaledWidth = 320;
+                encoder.BitmapTransform.ScaledHeight = 240;
+                encoder.BitmapTransform.Rotation = Windows.Graphics.Imaging.BitmapRotation.Clockwise90Degrees;
+                encoder.BitmapTransform.InterpolationMode = BitmapInterpolationMode.Fant;
+                encoder.IsThumbnailGenerated = true;
+
+                try
+                {
+                    await encoder.FlushAsync();
+                }
+                catch (Exception err)
+                {
+                    const int WINCODEC_ERR_UNSUPPORTEDOPERATION = unchecked((int)0x88982F81);
+                    switch (err.HResult)
+                    {
+                        case WINCODEC_ERR_UNSUPPORTEDOPERATION:
+                            // If the encoder does not support writing a thumbnail, then try again
+                            // but disable thumbnail generation.
+                            encoder.IsThumbnailGenerated = false;
+                            break;
+                        default:
+                            throw;
+                    }
+                }
+
+                if (encoder.IsThumbnailGenerated == false)
+                {
+                    await encoder.FlushAsync();
+                }
+
+
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+
+    }
+
+}
+

0da9cb94d86ee90eaa416510b1a23e68e344a3ea
janezdu <zd53@cornell.edu>
2021-06-30 17:33:03 -0400

    Simplified a name

--- Samples/AudioPreprocessing/WinUI/AudioPreprocessing/AudioPreprocessing/MainWindow.xaml.cs
+++ Samples/AudioPreprocessing/WinUI/AudioPreprocessing/AudioPreprocessing/MainWindow.xaml.cs
@@ -14,7 +14,7 @@ using Windows.Storage.Pickers;
 
 namespace AudioPreprocessing
 {
-    [ComImport, System.Runtime.InteropServices.Guid("3E68D4BD-7135-4D10-8018-9FB6D9F33FA1"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    [ComImport, Guid("3E68D4BD-7135-4D10-8018-9FB6D9F33FA1"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
     public interface IInitializeWithWindow
     {
         void Initialize([In] IntPtr hwnd);

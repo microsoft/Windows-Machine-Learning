e09cdd9f64619eec77e273b99fb12c3a3cccf292
nums11 <numsmt2@gmail.com>
2021-07-29 14:30:32 -0700

    Add file selector

--- Samples/WinMLSamplesGallery/WinMLSamplesGallery/Samples/BatchedInference.xaml.cs
+++ Samples/WinMLSamplesGallery/WinMLSamplesGallery/Samples/BatchedInference.xaml.cs
@@ -20,12 +20,43 @@ namespace WinMLSamplesGallery.Samples
     /// </summary>
     public sealed partial class BatchedInference : Page
     {
+        [DllImport("user32.dll", ExactSpelling = true, CharSet = CharSet.Auto, PreserveSig = true, SetLastError = false)]
+        public static extern IntPtr GetActiveWindow();
 
         public BatchedInference()
         {
             this.InitializeComponent();
         }
 
+        private void OpenButton_Clicked(object sender, RoutedEventArgs e)
+        {
+            var file = PickFile();
+            if (file != null)
+            {
+
+            }
+        }
+
+        private StorageFile PickFile()
+        {
+            FileOpenPicker openPicker = new FileOpenPicker();
+            openPicker.ViewMode = PickerViewMode.Thumbnail;
+            openPicker.FileTypeFilter.Add(".jpg");
+            openPicker.FileTypeFilter.Add(".bmp");
+            openPicker.FileTypeFilter.Add(".png");
+            openPicker.FileTypeFilter.Add(".jpeg");
+
+            // When running on win32, FileOpenPicker needs to know the top-level hwnd via IInitializeWithWindow::Initialize.
+            if (Window.Current == null)
+            {
+                var picker_unknown = Marshal.GetComInterfaceForObject(openPicker, typeof(IInitializeWithWindow));
+                var initializeWithWindowWrapper = (IInitializeWithWindow)Marshal.GetTypedObjectForIUnknown(picker_unknown, typeof(IInitializeWithWindow));
+                IntPtr hwnd = GetActiveWindow();
+                initializeWithWindowWrapper.Initialize(hwnd);
+            }
+
+            return openPicker.PickSingleFileAsync().GetAwaiter().GetResult();
+        }
 
     }
 }

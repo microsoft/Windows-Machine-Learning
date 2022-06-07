d47c415a908808ea4e27ed6accfc1a3b091d1a4c
nums11 <numsmt2@gmail.com>
2022-06-07 10:41:34 -0700

    Clean up frontend

--- Samples/WinMLSamplesGallery/WinMLSamplesGallery/Samples/DXResourceBindingORT/DXResourceBindingORT.xaml.cs
+++ Samples/WinMLSamplesGallery/WinMLSamplesGallery/Samples/DXResourceBindingORT/DXResourceBindingORT.xaml.cs
@@ -10,7 +10,6 @@ using Windows.Media;
 using Windows.Storage;
 using WinMLSamplesGalleryNative;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace WinMLSamplesGallery.Samples
 {
@@ -31,73 +30,74 @@ namespace WinMLSamplesGallery.Samples
         public DXResourceBindingORT()
         {
             this.InitializeComponent();
-            //WinMLSamplesGalleryNative.DXResourceBinding.BindDXResourcesUsingORT();
         }
 
         private async void LaunchWindow(object sender, RoutedEventArgs e)
         {
-            LaunchWindowBtn.IsEnabled = false;
-            LoadingTxt.Visibility = Visibility.Visible;
+            ToggleLaunchWindowButton();
+            ToggleLoadingTxtVisibility();
+
             await Task.Run(() => WinMLSamplesGalleryNative.DXResourceBinding.LaunchWindow());
-            LoadingTxt.Visibility = Visibility.Collapsed;
 
+            ToggleLoadingTxtVisibility();
+            ClassifyCube();
+        }
 
-            //WinMLSamplesGalleryNative.DXResourceBinding.EvalORT();
-            //System.Threading.Thread.Sleep(2000);
-
+        private async void ClassifyCube()
+        {
             int i = 0;
-            while(true)
+            while (true)
             {
-                if(pageExited)
+                if (pageExited)
                 {
                     WinMLSamplesGalleryNative.DXResourceBinding.CloseWindow();
                     break;
                 }
                 float[] results = await Task.Run(() => WinMLSamplesGalleryNative.DXResourceBinding.EvalORT());
-                UpdateClassification(results);
+                UpdateClassificationResults(results);
                 System.Diagnostics.Debug.WriteLine(i.ToString() + ": Updated ui with eval\n");
                 System.Threading.Thread.Sleep(1000);
                 i++;
             }
-
-
-
-            //System.Diagnostics.Debug.WriteLine("In C# code");
-            //for(int i = 0; i < results_lst.Count; i++)
-            //{
-            //    System.Diagnostics.Debug.WriteLine("indicies[i]: " + indices[i].ToString());
-            //    //System.Diagnostics.Debug.WriteLine(i.ToString() + ": " + results_lst[i].ToString());
-            //}
-            LaunchWindowBtn.IsEnabled = true;
+            ToggleLaunchWindowButton();
             pageExited = false;
         }
 
-        //private async Task<float[]> ClassifyFrame()
-        //{
-        //    return WinMLSamplesGalleryNative.DXResourceBinding.EvalORT();
-        //}
-
-        void UpdateClassification(float[] results)
+        private void UpdateClassificationResults(float[] results)
         {
             var results_lst = new List<float>(results);
-            List<int> indices = Enumerable.Range(0, 1000).ToList();
-            indices.Sort((x, y) => results_lst[y].CompareTo(results_lst[x]));
-            List<int> top_10_indices = indices.Take(10).ToList();
+            List<int> top_1k_imagenet_indices = Enumerable.Range(0, 1000).ToList();
+            top_1k_imagenet_indices.Sort((x, y) => results_lst[y].CompareTo(results_lst[x]));
+            // The top 10 results represented by their indices in the list of ImageNet labels
+            List<int> top_10_imagenet_indices = top_1k_imagenet_indices.Take(10).ToList();
 
-            List<DXClassifierResult> final_results = new List<DXClassifierResult>();
+            List<DXClassifierResult> dx_classifier_results = new List<DXClassifierResult>();
             for (int i = 0; i < 10; i++)
             {
-                final_results.Add(new DXClassifierResult(i+1, ClassificationLabels.ImageNet[top_10_indices[i]]));
+                dx_classifier_results.Add(new DXClassifierResult(i+1,
+                    ClassificationLabels.ImageNet[top_10_imagenet_indices[i]]));
             }
 
-            BaseExample.ItemsSource = final_results;
+            Top10Results.ItemsSource = dx_classifier_results;
+        }
+
+        private void ToggleLaunchWindowButton()
+        {
+            LaunchWindowBtn.IsEnabled = !LaunchWindowBtn.IsEnabled;
+        }
+
+        private void ToggleLoadingTxtVisibility()
+        {
+            if (LoadingTxt.Visibility == Visibility.Visible)
+                LoadingTxt.Visibility = Visibility.Collapsed;
+            else
+                LoadingTxt.Visibility = Visibility.Visible;
         }
 
         public void StopAllEvents()
         {
             System.Diagnostics.Debug.WriteLine("In dx... stop all events called");
             pageExited = true;
-            //WinMLSamplesGalleryNative.DXResourceBinding.CloseWindow();
         }
     }
 }
